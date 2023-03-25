25130b3bfb4ca657e4aa2022b3ceaaae195b6426
Kamrul Hasan <kamrulcse7@gmail.com>
2023-03-25 23:42:27 +0600

    map, loop, var, dynamic, string

---
+++ 20230325/main.dart
@@ -0,0 +1,62 @@
+// map in dart
+
+void main() {
+
+  // List id = [36, 37, 38, 39, 45, 65, 15];
+
+  // // for (int i = 0; i < id.length; i++) {
+  // //   print(id[i]);
+  // // }
+
+  // // for (var i in id) {
+  // //   print(i);
+  // // }
+
+  // // int i = 0;
+  // // do {
+  // //   print(id[i]);
+  // //   i++;
+  // // } while (i < id.length);
+
+  // var i = 0;
+  // while(i < id.length){
+  //   // i++;
+  //   print(id[i]);
+  //   i++;
+  // }
+  
+
+    Map<String, dynamic> student_Info = {"name": "Kamrul"};
+
+  List info = [
+    {
+      'name': "kamrul",
+    },
+    {
+      'name': "Emon",
+    },
+    {
+      'name': "Hasan",
+    },
+    {
+      'name': "Evan",
+    },
+    {
+      'name': "Raihan",
+    },
+  ];
+
+// for(int i = 0 ; i < student_Info.length; i++){
+//   print(student_Info);
+//   print(student_Info.keys);
+//   print(student_Info.values);
+// }
+
+for(int i = 0; i < info.length; i++){
+  // print(info[i]);
+  // print(info[i].values);
+  // print(info[i].keys);
+}
+
+
+}
