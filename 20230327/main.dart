23b471abcbae31d01efe56cd24fa7133f0cf1e84
Kamrul Hasan <kamrulcse7@gmail.com>
2023-04-01 22:38:19 +0600

    flutter project create, dart number data type(int, double)

---
+++ 20230327/main.dart
@@ -0,0 +1,39 @@
+void main() {
+  List student = [
+    {
+      "name": "Biva Afra",
+      "roll": 09,
+    },
+    {
+      "name": "Mahamudul",
+      "roll": 28,
+    },
+    {
+      "name": "Md. Ahasun Mia",
+      "roll": 06,
+    },
+    {
+      "name": "RK Mikkat",
+      "roll": 11,
+    },
+    {
+      "name": "Abdulla al noman",
+      "roll": 27,
+    },
+  ];
+
+  // for(var i = 0; i < student.length; i++){
+  // var name = student[i]['name'];
+  //   // print(item);
+  //   if(name == "Md. Ahasun Mia"){
+  //     // print("06");
+  //     name = "Emon";
+  //   }else{
+  //     // print(name);
+  //   }
+  //   print(name);
+  // }
+
+  var fullName = "Kamrul Hasan";
+  print(fullName.contains("Kamrul Hasan"));
+}
---
+++ 20230401/main.dart
@@ -0,0 +1,47 @@
+void main() {
+  // number data type (2 type of number)
+  // int a;
+  // a = 12;
+
+  // int b = 25;
+  // a = 28;
+
+  // print(a);
+  // print(b);
+
+  // int a, b;
+  // a = 25;
+  // b = 36;
+  // int sum = a + b;
+  // print(sum);
+
+  // int sub = a - b;
+  // print(sub);
+
+  // int multi = a * b;
+  // print(multi);
+
+  // double div = a / b;
+  // print(div);
+
+  // double gpa = 3.68;
+  // double lat = 1.22455;
+  // double long = 0.25555;
+
+  // print(gpa);
+  // print(gpa.toInt()); // convert double to int
+  // print(gpa.round()); // round fig
+  // print(lat.toStringAsFixed(2));
+
+  // int n = 12;
+  // print(n.toDouble());
+
+  // print(n + gpa);
+
+// string data type
+  String name;
+  name = 'Kamrul Hasan';
+  print(name);//
+  name = "Rakib";
+  print(name); //
+}
