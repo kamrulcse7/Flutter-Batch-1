a765c67000442af27890c6e2466cc0d0aa6d4be4
Kamrul Hasan <kamrulcse7@gmail.com>
2023-04-03 23:03:37 +0600

    string, booleans, const, final

---
+++ 20230403/main.dart
@@ -0,0 +1,58 @@
+void main() {
+  // print("Hello world");
+  //  String name = "Rakib";
+  //  String name2;
+  //  name2 = "Hasan";
+
+  //  name2 = 'Mahmud';
+
+  //  print(name);
+  //  print(name2);
+
+  // String fName = "Kamrul";
+  // String lName = "Hasan";
+  // String nName = "Rasel";
+  // print(fName + lName);
+  // print(fName + ' ' + lName);
+  // print("$fName $lName");
+  // print("${fName} ${lName}");
+  // print("${fName} ${lName} ${nName}");
+
+  // String fName = "Rakib";
+  // String lName = "Mahmud";
+  // int age = 26;
+  // double gpa = 4.00;
+
+  // String info = "FName: ${fName}\nLName: ${lName}\nAge: ${age}\nGPA: ${gpa}";
+  // String userAge = age.toString();
+  // print(info);
+
+  // print('I\'m Kamrul Hasan');
+  // print("I'm Kamrul Hasan");
+  // print('I"m kamrul');
+
+  // String name = "Raihan";
+  // String name2 = "Rabia";
+  // bool isMale = true;
+  // bool isFemale = true;
+
+  // print("Name : " + name + " Male : " + isMale.toString());
+  // print("Name : " + name + " FeMale : " + isFemale.toString());
+
+  var a = 526;
+  var b = "DIU";
+  var c = 3.2;
+  var d = true;
+
+// const and final 
+const number = 26;
+// number = 25;
+
+final uni = "DIU";
+//  uni = "UIU";
+
+final name;
+name = "Mahmud";
+// name = "Raihan";
+
+}
