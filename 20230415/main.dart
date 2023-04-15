ee103050055b3e6e58b44ad998f56a8c504323a4
Kamrul Hasan <kamrulcse7@gmail.com>
2023-04-15 23:16:20 +0600

    if, else if, else

---
+++ 20230415/main.dart
@@ -0,0 +1,109 @@
+void main() {
+  // var age = 20;
+
+  // if(age >= 20){
+  //   print("Age > 20");
+  // }else{
+  //   print("Age < 20");
+  // }
+
+  // double mark = 63;
+
+  // if (mark < 40) {
+  //   print("Fail");
+  // } else {
+  //   print("Pass");
+  // }
+
+// 0 - 32: fail
+// 33 - 39 : D
+// 40 - 49 : C
+// 50 - 59 : B
+// 60 - 69 : A-
+// 70 - 79 : A
+// 80 - 100 : A+
+
+  // double mark = 36;
+  // if(mark < 33){
+  //   print("Fail");
+  // }else if(mark < 40){
+  //   print("D");
+  // }else if(mark < 50){
+  //   print("C");
+  // }else if(mark  < 60){
+  //   print("B");
+  // }else if (mark < 70){
+  //   print("A-");
+  // }else if(mark < 80){
+  //   print("A");
+  // }else if(mark <= 100){
+  //   print("A+");
+  // }else{
+  //   print("Invalid");
+  // }
+
+  // String name = "Raihan";
+  // if(name == 'Raihan'){
+  //   print("Name : $name");
+  // }else{
+  //   print("Name not found");
+  // }
+
+  // var name = "raihan";
+  // bool male = true;
+
+  // if (male == true) {
+  //   print("Male");
+  // } else {
+  //   print("Female");
+  // }
+
+  // List studentInfo = [
+  //   {
+  //     "name": "Raihan",
+  //     "male": true,
+  //   },
+  //   {
+  //     "name": "Emon",
+  //     "male": true,
+  //   },
+  //   {
+  //     "name": "Jorina",
+  //     'male': false,
+  //   }
+  // ];
+
+  // if(studentInfo[1]['male'] == true ){
+  //   print("Male");
+  // }else{
+  //   print("Female");
+  // }
+
+
+  // if(studentInfo[0]['name'] == 'Emon' && studentInfo[0]['male'] == true){
+  //   print("Name : ${studentInfo[0]['name']}");
+  //   print("Male : ${studentInfo[0]['male']}");
+  // }else{
+  //   print("Data not found");
+  // }
+
+  
+  // if(studentInfo[1]['name'] == 'Emon' || studentInfo[2]['male'] == false){
+  //   print("Name : ${studentInfo[0]['name']}");
+  //   print("Male : ${studentInfo[0]['male']}");
+  // }else{
+  //   print("Data not found");
+  // }
+
+
+  var isMale = true;
+  // print(isMale);
+  // print(isMale != true);
+
+  if(isMale != true){
+    print("Female");
+  }else{
+    print("Male");
+  }
+
+}
